4f082abd139c7e74ad31488b89dbc9d854d2fdb8
Salli 95 - Vivobook <ravileviskander@gmail.com>
2024-05-06 19:53:39 +0500

    почти все

--- Password random.py
+++ Password random.py
@@ -1,8 +1,35 @@
+# нужно сделать настроку типа пароля а также его длину
+
 import random
 import string
 
-# LARGletters=()
-SMALLletters=(string.ascii_lowercase)
+LARGletters = string.ascii_uppercase
+SMALLletters = string.ascii_lowercase
+INTEGER = string.digits
 
-# def generate_password(length):
-print(SMALLletters)
+print("Выберите тип пароля: \n1-смешаный\n2-численый\n3-символьный")
+answer=int(input())
+
+def mixedpassword(length):
+    password = ''
+    for i in range(length):
+        
+     if answer==1: 
+      fhg = random.choice([LARGletters, SMALLletters, INTEGER])
+      password += random.choice(fhg)
+      
+     if answer==2:
+       fhg = random.choice([INTEGER])
+       password += random.choice(fhg)
+          
+     if answer==3:
+       fhg = random.choice([LARGletters, SMALLletters])
+       password += random.choice(fhg)
+              
+          
+    return password
+
+
+
+
+print(mixedpassword(10))
